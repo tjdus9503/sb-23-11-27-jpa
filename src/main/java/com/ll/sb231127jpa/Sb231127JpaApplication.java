168f834c2dcb5c7ccfcc81681469cf34cc30c656
tjdus9503 <tjdus03095@gmail.com>
2023-12-02 18:13:21 +0900

    setting

---
+++ src/main/java/com/ll/sb231127jpa/Sb231127JpaApplication.java
@@ -0,0 +1,13 @@
+package com.ll.sb231127jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb231127JpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb231127JpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sb231127jpa/Sb231127JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sb231127jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb231127JpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
